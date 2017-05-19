11d20d7d5b7d70e9c2ce81d6aa3e8d3cc7407423
augustelaa <augusto_conceicao@hotmail.com>
2017-05-19 15:01:15 -0300

    Implementação da validação para o form de nova task. Correção no boolean flutuante da situação. Adição de comentários em locais específicos das rotinas.

--- client/src/app/tasks/tasks.component.ts
+++ client/src/app/tasks/tasks.component.ts
@@ -9,14 +9,14 @@ import { TasksService } from './tasks.service';
 })
 export class TasksComponent implements OnInit {
 
-  tasks: Task[] = [];
-  task: Task;
-  busy: Boolean;
+  tasks: Task[] = []; // responsável por alimentar a lista de tarefas
+  task: Task; // responsável por transportar os dados do form de adição
+  busy: Boolean; // responsável por habilitar/desabilitar submit do form de adição (evitar duplo envio)
   
   constructor(public tasksService: TasksService) {  }
 
   ngOnInit() { 
-    this.retornarTasks(); 
+    this.retornarTasks();
     this.task = new Task(); 
     this.busy = false;
   }
@@ -45,25 +45,35 @@ export class TasksComponent implements OnInit {
   }
 
   alterarTask(_task: Task){
-    this.tasksService.alterarTask(_task).subscribe(res => { if(res.status == "ok"){} });
+    this.tasksService.alterarTask(_task).subscribe(res => { if(res.status == "ok"){this.retornarTasks();} });
   }
 
+  /*
+   * Função específica para troca de situação, criada para evitar rotinas desnecessárias na alterarTask.
+   */
   alterarSituacaoTask(_task: Task){
-    _task.situacao = !_task.situacao;
+    if (_task.situacao == false) {
+      _task.situacao = true;
+    }else{
+      _task.situacao = false;
+    }
     this.alterarTask(_task);
   }
 
 }
 
+/*
+ * Classe Task é a estrutura de todos os dados da tela de tasklist.
+ */
 export class Task {
   
   codigo: Number;
   titulo: String;
   descricao: String;
   situacao: Boolean;
-  dataHoraCriacao: String;
-  dataHoraEncerramento: String;
-  dataHoraUltimaAlteracao: String;
+  dataHoraCriacao: String; // A base automáticamente insere "CURRENT_TIMESTAMP" ao realizar o INSERT
+  dataHoraEncerramento: String; // A base automáticamente insere "CURRENT_TIMESTAMP" ao realizar a troca de situação via trigger
+  dataHoraUltimaAlteracao: String; // A base automáticamente insere "CURRENT_TIMESTAMP" ao realizar a mudança de dados via trigger
 
 
   constructor(){
--- client/src/app/tasks/tasks.service.ts
+++ client/src/app/tasks/tasks.service.ts
@@ -8,36 +8,38 @@ import {Task} from './tasks.component'
 @Injectable()
 export class TasksService {
 
-constructor(private http: Http) { }
+  backend_url: string; // Variável criada para facilitar futuras mudanças
+
+constructor(private http: Http) { this.backend_url = 'http://tasklistapi.esy.es/public/tasks'; }
 
 private extractData(res) {
 
   if (res.status < 200 || res.status >= 300) {
-      throw new Error('Bad response status: ' + res.status);
+      throw new Error('Não foi possível intermediar o acesso ao backend. Código do erro: ' + res.status);
     }
     return (res.json()) || {};
   }
 
   retornarTasks(): Observable<any> {
-    return this.http.get('http://tasklistapi.esy.es/public/tasks').map(this.extractData);
+    return this.http.get(this.backend_url).map(this.extractData);
   }
 
   retornarTask(codigo: Number): Observable<any> {
-    return this.http.get('http://tasklistapi.esy.es/public/tasks/' + codigo).map(this.extractData);
+    return this.http.get(this.backend_url + '/' + codigo).map(this.extractData);
   }
 
   adicionarTask(task: Task): Observable<any> {
     let options = JSON.stringify({ titulo: task.titulo, descricao: task.descricao, situacao: task.situacao });
-    return this.http.post('http://tasklistapi.esy.es/public/tasks', options).map(this.extractData);
+    return this.http.post(this.backend_url, options).map(this.extractData);
   }
   
   removerTask(codigo: Number): Observable<any> {
-    return this.http.delete('http://tasklistapi.esy.es/public/tasks/' + codigo).map(this.extractData);
+    return this.http.delete(this.backend_url + '/' + codigo).map(this.extractData);
   }
 
   alterarTask(task: Task): Observable<any> {
     let options = JSON.stringify({ titulo: task.titulo, descricao: task.descricao, situacao: task.situacao });
-    return this.http.put('http://tasklistapi.esy.es/public/tasks/' + task.codigo, options).map(this.extractData);
+    return this.http.put(this.backend_url + '/' + task.codigo, options).map(this.extractData);
   }
 
 }
